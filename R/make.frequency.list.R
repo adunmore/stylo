a0e2eee7c9154c7482ec9fdeb4b68c4d87ba071b
aedunmore <aedunmore@gmail.com>
2019-11-19 22:57:32 -0500

    Improved version of make.frequency.list
    Implements two main changes that radically improve performance on a large dataset.
    
    - using unlist() with use.names = FALSE results in huge performance gains
    - calculating frequency table with dplyr::summarise() rather than table() results in modest but still worthwhile improvements.

--- R/make.frequency.list.R
+++ R/make.frequency.list.R
@@ -6,61 +6,56 @@
 # Refer to help(make.frequency.list) for farther details.
 # #################################################
 
-make.frequency.list = function(data, 
-                               value = FALSE,
-                               head = NULL,
-                               relative = TRUE) {
-     
-                                       
-                                       
-     #####################################
-     # first, sanitize the input dataset
-     
-     # test if the dataset belongs to 'stylo.corpus' class
-     if(class(data) == "stylo.corpus" | is.list(data) == TRUE) {
-             # unlist, or make one long text out of the corpus samples
-             data = unlist(data)    
-     # otherwise, test if the dataset is a vector
-     } else if(is.vector(data) == FALSE) {        
-             # whet it is not, produce an error message and stop
-             stop("unable to make a list of frequencies")
-     }
-     
-     # test if the dataset has at least two elements
-     if(length(data) < 3) {
-             stop("you try to measure frequencies of an empty vector!")
-     }
-     #####################################
-
-
-     
-     #####################################     
-     # the dataset sanitized, let counting the features begin!     
-     frequent.features = sort(table(data), decreasing = TRUE)
-     #####################################
-
-     
-     # if relative frequencies were requested, they are normalized
-     if(relative == TRUE) {
-             frequent.features = frequent.features / length(data) * 100
-     }
-     
-     # additionally, one might limit the number of the most frequent features;
-     # this will return first n elements only (this is the argument 'head')
-     if(is.numeric(head) == TRUE) {
-             # sanitizing the indicated number
-             head = abs(round(head))
-             if(head == 0) head = 1
-             # cutting off the list
-             frequent.features = frequent.features[1:head]
-     }
-     
-     # in most cases, one needs just a list of features, without frequencies
-     if(value == FALSE) {
-             frequent.features = names(frequent.features)
-     }
-     
-     
-return(frequent.features)     
+make.frequency.list <- function(data,
+                                value = FALSE,
+                                head = NULL,
+                                relative = TRUE) {
+        
+        #####################################
+        # first, sanitize the input dataset
+        
+        # test if the dataset belongs to 'stylo.corpus' class
+        if(class(data) == "stylo.corpus" | is.list(data) == TRUE) {
+                # unlist, or make one long text out of the corpus samples
+                data <- enframe(unlist(data, FALSE, FALSE))    
+                # otherwise, test if the dataset is a vector
+        } else if(is.vector(data) == FALSE) {        
+                # whet it is not, produce an error message and stop
+                stop("unable to make a list of frequencies")
+        }
+        
+        # test if the dataset has at least two elements
+        if(nrow(data) < 3) {
+                stop("you try to measure frequencies of an empty vector!")
+        }
+        #####################################
+        
+        # count each feature and sort them by descending rate of occurrence
+        frequent.features <- data %>% 
+                group_by(value) %>% 
+                summarise(result = n()) %>% 
+                arrange(desc(result))
+        
+        # calculate relative frequencies, if requested
+        if(relative == TRUE) {
+                frequent.features <- frequent.features %>% 
+                        mutate(result = (result / nrow(data)) * 100)
+        }
+        
+        # return the top n features, if specified
+        if(!is.null(head)) {
+                frequent.features <- top_n(frequent.features, head)
+        }
+        
+        # only return rates of occurrence if requested
+        if(value == FALSE) {
+                result_list <- frequent.features$value
+        } else {
+                result_list <- frequent.features$result
+                names(result_list) <- frequent.features$value
+                result_list <- as.table(result_list)
+        }
+        
+        return(result_list)
 }
 
